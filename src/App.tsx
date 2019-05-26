41661a88f04610d5fee386c67f4fe921e3281271
Martin <mr.mmaurer+work@gmail.com>
2019-05-26 21:55:17 +0200

    Added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import Home from 'pages/Home';
 import Entry from 'pages/Entry';
 import Barcode from 'pages/Barcode';
 
-import './App.css';
+import styles from './app.module.css';
+import './global.css';
 
 const App: React.FunctionComponent = () => (
   <Router>
-    <div className="app">
-      <header className="header">
-        <Link to="/">Home</Link>
-        <Link to="/entry">Entry</Link>
-        <Link to="/barcode">Barcode</Link>
+    <div className={ styles.wrapper }>
+      <header className={ styles.header }>
+        <h1 className={ styles['header-title'] }>
+          PSTQR
+        </h1>
       </header>
-      <main className="main">
+      <nav className={ styles.nav }>
+        <Link className={ styles['nav-link'] } to="/">Home</Link>
+        <Link className={ styles['nav-link'] } to="/entry">Entry</Link>
+        <Link className={ styles['nav-link'] } to="/barcode">Home</Link>
+      </nav>
+      <main className={ styles.main }>
         <Route exact path="/" component={Home} />
         <Route path="/entry" component={Entry} />
         <Route path="/barcode" component={Barcode} />
       </main>
+      <footer className={ styles.footer }>
+        Footer
+      </footer>
     </div>
   </Router>
 );
